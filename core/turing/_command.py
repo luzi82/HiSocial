ffe75548d2948232c0678a483e90fdacfe1c8f7d
luzi82 <luzi82@gmail.com>
2012-02-28 01:05:59 +0800

    add checking on public_guest_test_turing for security

--- core/turing/_command.py
+++ core/turing/_command.py
@@ -4,12 +4,18 @@ import core_config
 import json
 
 def public_guest_test_turing(turing_input,_ip):
-    a=""
-    try:
-        v = json.loads(turing_input)
-        output = captcha.submit(v["challenge"], v["response"], core_config.RECAPTCHA_PRIVATE_KEY, _ip)
-        if not output.is_valid:
-            return Command.fail(turing_input)
-        return Command.ok()
-    except Exception:
+    if not isinstance(turing_input, str):
         return Command.fail()
+    if not isinstance(_ip, str):
+        return Command.fail()
+    v = json.loads(s=turing_input,encoding="utf8")
+    if not isinstance(v, dict):
+        return Command.fail()
+    if not isinstance(v["challenge"], unicode):
+        return Command.fail()
+    if not isinstance(v["response"], unicode):
+        return Command.fail()
+    output = captcha.submit(v["challenge"], v["response"], core_config.RECAPTCHA_PRIVATE_KEY, _ip)
+    if not output.is_valid:
+        return Command.fail(reason="not valid")
+    return Command.ok()
