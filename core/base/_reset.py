d535c6b7812b2c82e3283d3ded98772685cc9f90
luzi82 <luzi82@gmail.com>
2012-02-29 22:52:48 +0800

    fix bug of cannot clean more than 1 tables

--- core/base/_reset.py
+++ core/base/_reset.py
@@ -8,11 +8,16 @@ def clean():
     trace_up("Clean database")
     db = Database.connect_mysqldb()
     db.query("SHOW TABLES")
-    table_list = db.store_result().fetch_row()
-    for r in table_list :
-        table_name=r[0]
-        trace("Remove table: "+table_name)
-        db.query("DROP TABLE "+table_name)
+    result = db.store_result()
+    
+    while True:
+        row = result.fetch_row()
+        if row == None:break
+        if len(row)==0:break
+        for i in row:
+            table_name = i[0]
+            trace("Remove table: "+table_name)
+            db.query("DROP TABLE `"+table_name+"`")
     trace_down("done")
     db.commit()
     db.close()
