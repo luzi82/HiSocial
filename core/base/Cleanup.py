c3b81fa08d4eb678f3c2e36b062496dd933431b5
luzi82 <luzi82@gmail.com>
2012-02-29 23:26:15 +0800

    add the cleanup class

---
+++ core/base/Cleanup.py
@@ -0,0 +1,22 @@
+class Cleanup(object):
+
+    cleanup_stack = []
+    
+    def __init__(self):
+        pass
+    
+    def __del__(self):
+        self.close()
+    
+    def push(self, func):
+        self.cleanup_stack.append(getattr(func))
+
+    def clean(self):
+        self.pop()()
+
+    def pop(self):
+        return self.cleanup_stack.pop()
+
+    def close(self):
+        while len(self.cleanup_stack) > 0:
+            self.clean()
