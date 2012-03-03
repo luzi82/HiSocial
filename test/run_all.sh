ab9dfcca7e3456262b1f728f907842c0eef607d4
luzi82 <luzi82@gmail.com>
2012-03-03 16:37:50 +0800

    test remove dependence of tool folder src

--- test/run_all.sh
+++ test/run_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PYTHONPATH=../core:../tool nosetests *.py
+PYTHONPATH=../core nosetests *.py
--- test/user_reset.py
+++ test/user_reset.py
@@ -1,5 +1,4 @@
 import unittest
-import install_config
 from user import User,UserGroup,Permission
 from base.Cleanup import Cleanup
 from base import Database, Runtime
@@ -8,16 +7,22 @@ from base.Runtime import trace
 
 class TestUserPackage(unittest.TestCase):
     
+    ic = None
+    
     def setUp(self):
-        Runtime.enable_trace = True
-        reset.reset(install_config)
+        class icc(object):
+            OWNER_USERNAME="akari"
+            OWNER_PASSWORD="mizunashi"
+        self.ic=icc()
+        Runtime.enable_trace = False
+        reset.reset(self.ic)
         Runtime.enable_trace = True
     
     def test_owner(self):
         cleanup = Cleanup()
         session = Database.create_sqlalchemy_session_push(cleanup)
         
-        self.assertTrue(User.check_user_account_exist(session,install_config.OWNER_USERNAME))
-        self.assertTrue(User.check_user_account_password(session,install_config.OWNER_USERNAME,install_config.OWNER_PASSWORD))
-        self.assertTrue(UserGroup.get_group(session,install_config.OWNER_USERNAME),["owner"])
-        self.assertTrue(Permission.get_user_permission(session, install_config.OWNER_USERNAME, "admin"))
+        self.assertTrue(User.check_user_account_exist(session,self.ic.OWNER_USERNAME))
+        self.assertTrue(User.check_user_account_password(session,self.ic.OWNER_USERNAME,self.ic.OWNER_PASSWORD))
+        self.assertTrue(UserGroup.get_group(session,self.ic.OWNER_USERNAME),["owner"])
+        self.assertTrue(Permission.get_user_permission(session, self.ic.OWNER_USERNAME, "admin"))
