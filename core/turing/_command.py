a15174d3e5a77b057f66cb1726536303f97ebcf0
luzi82 <luzi82@gmail.com>
2012-03-03 16:06:40 +0800

    change turing filename

--- core/turing/_command.py
+++ core/turing/_command.py
@@ -1,7 +1,7 @@
-from turing import check
+import turing
 from base import Command
 
 def public_guest_test_turing(turing_input,_ip):
-    if not check.check_recaptcha(turing_input,_ip):
+    if not turing.check_recaptcha(turing_input,_ip):
         return Command.fail()
     return Command.ok()
