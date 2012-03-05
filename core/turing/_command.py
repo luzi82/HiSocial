1eaa168f055bcb554f2c9a9400cc0e41ce005781
luzi82 <luzi82@gmail.com>
2012-03-05 18:05:07 +0800

    add doc in core/turing/_command.py

--- core/turing/_command.py
+++ core/turing/_command.py
@@ -2,6 +2,17 @@ import turing
 from base import Command
 
 def public_guest_test_turing(turing_value,_ip):
+    '''
+    Check recaptcha, wrapper of turing.check_recaptcha.
+    
+    @type  turing_value: str
+    @param turing_value: turing value in JSON format, should have turing_value["challenge"] and turing_value["response"]
+    @type  _ip: str
+    @param _ip: IP of client
+    
+    @rtype: dict
+    @return: Command response, OK iff success
+    '''
     if not turing.check_recaptcha(turing_value,_ip):
         return Command.fail()
     return Command.ok()
