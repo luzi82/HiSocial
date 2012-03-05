6f97d593415d890c4f7119765c6606109f2f19dd
luzi82 <luzi82@gmail.com>
2012-03-05 17:53:48 +0800

    add doc to core/base/_command.py

--- core/base/_command.py
+++ core/base/_command.py
@@ -3,15 +3,23 @@ import string
 import Version
 
 def public_guest_get_version():
+    """
+    Return the HiSocial version info
+    
+    @rtype: dict
+    @return: ret["version"] current version. ret["type"] "development" or "production"
+    """
     return Command.ok({"version":Version.VERSION, "type":Version.TYPE})
 
 def public_guest_ping(value):
     '''
     Ping the system
     
-    :param value: len-8 hex string
+    @type value: str
+    @param value: len-8 hex string
     
-    :return: value=~(value), len-8 hex string.  No value if bad input.
+    @rtype: dict
+    @return: ret["value"]=~(value), len-8 hex string.  No value if bad input.
     '''
     BAD_VALUE = Command.fail(reason="bad value")
     if(len(value) != 8):return BAD_VALUE
@@ -23,4 +31,11 @@ def public_guest_ping(value):
     return Command.ok({"value":t})
 
 def public_guest_list_cmd():
+    '''
+    List all command
+    wrapper of base.Command.list_cmd()
+    
+    @rtype: dict
+    @return: ["value"] = base.Command.list_cmd() output
+    '''
     return Command.ok({"value":Command.list_cmd()})
