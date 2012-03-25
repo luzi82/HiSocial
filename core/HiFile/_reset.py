4dc4c95a59b123d65cd76767bc4a4c2a6bf6fa1b
luzi82 <luzi82@gmail.com>
2012-03-25 18:11:22 +0800

    init DB for XBTT

---
+++ core/HiFile/_reset.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+import core_config
+
+def build_order():
+    return 20
+
+def build(install_config):
+    me=os.path.abspath(__file__)
+    me_path=os.path.dirname(me)
+    p1 = subprocess.Popen([
+                            "cat",
+                            me_path+"/xbt_tracker.sql"
+                           ],
+                          stdout=subprocess.PIPE)
+    p2 = subprocess.Popen([
+                            "mysql",
+                            "--host="+core_config.DB_SERVER,
+                            "--user="+core_config.DB_USERNAME,
+                            "--password="+core_config.DB_PASSWORD,
+                            "--default-character-set=utf8",
+                            core_config.DB_SCHEMATA
+                           ],
+                          stdin=p1.stdout,
+                          stdout=subprocess.PIPE)
+    p2.wait()
