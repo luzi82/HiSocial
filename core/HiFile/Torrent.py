e0484f9cff843410e02ca055fd8de6c3ea148c4c
luzi82 <luzi82@gmail.com>
2012-03-26 22:25:16 +0800

    parse_torrent support file input

--- core/HiFile/Torrent.py
+++ core/HiFile/Torrent.py
@@ -1,8 +1,12 @@
 from BitTorrent import bencode
 import hashlib
 
-def parse_torrent(torrent_filename):
-    return bencode.bdecode(open(torrent_filename,"rb").read())
+def parse_torrent(torrent):
+    if (isinstance(torrent, str)):
+        return bencode.bdecode(open(torrent,"rb").read())
+    elif (isinstance(torrent, file)):
+        return bencode.bdecode(torrent.read())
+    return None
 
 def get_info_hash_hex(torrent_data):
     return hashlib.sha1(bencode.bencode(torrent_data["info"])).hexdigest()
--- test/Torrent.py
+++ test/Torrent.py
@@ -10,7 +10,12 @@ class TestTorrent(unittest.TestCase):
     
     def test_parse(self):
         data=Torrent.parse_torrent("res/test0.torrent")
-#        pp.pprint(data)
+        info=data["info"]
+        info_hash_hex = hashlib.sha1(bencode.bencode(info)).hexdigest()
+        self.assertEqual(info_hash_hex, "2034385a2621c53a490f34c5893a860664741da4")
+        self.assertEqual(Torrent.get_info_hash_hex(data), "2034385a2621c53a490f34c5893a860664741da4")
+        
+        data=Torrent.parse_torrent(open("res/test0.torrent"))
         info=data["info"]
         info_hash_hex = hashlib.sha1(bencode.bencode(info)).hexdigest()
         self.assertEqual(info_hash_hex, "2034385a2621c53a490f34c5893a860664741da4")
