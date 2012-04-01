31cf40c923ead14b60933de16386f8d45ec2d44e
luzi82 <luzi82@gmail.com>
2012-04-01 17:35:21 +0800

    move function menu to upper 1 lv

--- web/hisocial/main/main.js
+++ web/hisocial/main/main.js
@@ -1,14 +1,11 @@
 function hi_main_show_main_view(){
 	unload_body();
 	$('body').load('hisocial/main/main.html #main_view',function(){
+		$("#main_panel_home_btn").click(hi_main_show_home);
 		$("#main_panel_logout_btn").click(hi_user_logout);
-		hi_main_show_main_menu();
 	});
 }
 
-function hi_main_show_main_menu(){
-	$('#main_content').load('hisocial/main/main.html #main_menu',function(){
-		//$("#main_menu_histatus_link").click(hi_user_logout);
-		//$("#main_menu_hifile_link").click(hi_user_logout);
-	});
+function hi_main_show_home(){
+	$('main_content').html("");
 }
