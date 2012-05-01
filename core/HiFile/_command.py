6ba8557ca5cda426db9f224ec1ba8a762b07dda8
luzi82 <luzi82@gmail.com>
2012-05-01 20:55:05 +0800

    remove unused import and comment

--- core/HiFile/_command.py
+++ core/HiFile/_command.py
@@ -2,18 +2,11 @@ import HiFile.Torrent
 from base import Command, Database
 from base.Cleanup import Cleanup
 import _database
-import pprint
-import user.UserLoginToken
 import TrackerManager
 from HiFile import TorrentStorage
 import time
-import HiFile
 
 def command_user_upload_torrent(txtf_user_token_user, file_torrent):
-#    actor_id = user.UserLoginToken.check_user_login_token(user_login_token)
-#    if actor_id == None:
-#        return Command.fail(reason="user_login_token")
-    
     file_bin = file_torrent.read()
     
     torrent_data = HiFile.Torrent.parse_torrent_data(file_bin)
@@ -35,10 +28,6 @@ def command_user_upload_torrent(txtf_user_token_user, file_torrent):
     return Command.ok(result={"torrent_id":torrent_id})
 
 def command_user_list_user_torrent(txtf_user_token_user, txt_user_id):
-#    actor_id = user.UserLoginToken.check_user_login_token(user_login_token)
-#    if actor_id == None:
-#        return Command.fail(reason="user_login_token")
-    
     if txt_user_id != txtf_user_token_user:
         return Command.fail(reason="no permission")
     
@@ -59,10 +48,6 @@ def command_user_list_user_torrent(txtf_user_token_user, txt_user_id):
     return Command.ok(result={"torrent_list":ret})
 
 def command_guest_get_torrent_data(txtf_HiFile_torrenttoken_torrent):
-#    ret = HiFile.verify_torrent_token(txt_torrent_token)
-#    if(ret == None):
-#        return Command.fail(reason="torrent_token",result={"torrent_token":txt_torrent_token})
-    
     cleanup = Cleanup()
     session = Database.create_sqlalchemy_session_push(cleanup)
     
@@ -75,8 +60,3 @@ def argfilter_torrenttoken(v):
     if(ret == None):
         return Command.fail(reason="bad torrent token")
     return Command.ok(value=ret)
-
-#def command_test(FILE_in):
-#    file_bin=FILE_in.read()
-#    torrent_data = HiFile.Torrent.parse_torrent_data(file_bin)
-#    return Command.ok(result={"info_hash_hex":HiFile.Torrent.get_info_hash_hex(torrent_data)})
