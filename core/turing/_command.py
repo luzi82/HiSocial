c344dd0509826f507d5b6f04456fa3307b9de0e6
luzi82 <luzi82@gmail.com>
2012-05-01 19:07:33 +0800

    fix comment

--- core/turing/_command.py
+++ core/turing/_command.py
@@ -7,8 +7,8 @@ def command_guest_test_turing(txtf_turing_turing,env_ip):
     
     @type  turing_value: str
     @param turing_value: turing value in JSON format, should have turing_value["challenge"] and turing_value["response"]
-    @type  _ip: str
-    @param _ip: IP of client
+    @type  env_ip: str
+    @param env_ip: IP of client
     
     @rtype: dict
     @return: Command response, OK iff success
