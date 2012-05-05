df158eb1f128bfae4a463d9afad4f4c1f5163001
luzi82 <luzi82@gmail.com>
2012-05-05 18:51:01 +0800

    add test case to web access test API

--- test/web_basic.py
+++ test/web_basic.py
@@ -7,3 +7,13 @@ class TestWebBasic(_testfunc.TestFunc):
         data=self.call_web({'PKG': "base", 'CMD': "guest_ping", 'txt_value': "0000ffff"})
         self.assertEqual(data[Command.RESULT_KEY], Command.RESULT_VALUE_OK_TXT)
         self.assertEqual(data["value"], "ffff0000")
+
+    def test_helloworld(self):
+        data=self.call_web({'PKG': "test", 'CMD': "helloworld"})
+        self.assertEqual(data[Command.RESULT_KEY], Command.RESULT_VALUE_OK_TXT)
+        self.assertEqual(data["value"], "helloworld")
+
+    def test_uppercase(self):
+        data=self.call_web({'PKG': "test", 'CMD': "uppercase", "txt_a":"asdf"})
+        self.assertEqual(data[Command.RESULT_KEY], Command.RESULT_VALUE_OK_TXT)
+        self.assertEqual(data["value"], "ASDF")
