9a9bdf4a3b04ba4e16500a42cba92157434416c2
luzi82 <luzi82@gmail.com>
2012-05-06 11:28:49 +0800

    move param to test common

--- test/user_reset.py
+++ test/user_reset.py
@@ -4,22 +4,20 @@ from base.Cleanup import Cleanup
 from base import Database, Runtime
 from admin import reset
 from base.Runtime import trace
+import _testcommon
 
 class TestUserPackage(unittest.TestCase):
     
-    OWNER_USERNAME="akari"
-    OWNER_PASSWORD="mizunashi"
-
     def setUp(self):
         Runtime.enable_trace = False
-        reset.reset(self)
+        reset.reset(_testcommon)
         Runtime.enable_trace = True
     
     def test_owner(self):
         cleanup = Cleanup()
         session = Database.create_sqlalchemy_session_push(cleanup)
         
-        self.assertTrue(User.check_user_account_exist(session,self.OWNER_USERNAME))
-        self.assertTrue(User.check_user_account_password(session,self.OWNER_USERNAME,self.OWNER_PASSWORD))
-        self.assertTrue(UserGroup.get_group(session,self.OWNER_USERNAME),["owner"])
-        self.assertTrue(Permission.get_user_permission(session, self.OWNER_USERNAME, "admin"))
+        self.assertTrue(User.check_user_account_exist(session,_testcommon.OWNER_USERNAME))
+        self.assertTrue(User.check_user_account_password(session,_testcommon.OWNER_USERNAME,_testcommon.OWNER_PASSWORD))
+        self.assertTrue(UserGroup.get_group(session,_testcommon.OWNER_USERNAME),["owner"])
+        self.assertTrue(Permission.get_user_permission(session, _testcommon.OWNER_USERNAME, "admin"))
