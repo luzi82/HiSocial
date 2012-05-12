99d1018964dadef0b435ea605af8c933f5707274
luzi82 <luzi82@gmail.com>
2012-05-12 21:06:09 +0800

    add test for cleanup func

---
+++ test/cleanup_test.py
@@ -0,0 +1,51 @@
+import _testcommon
+import base.Cleanup
+
+class Dummy(object):
+
+    x_count=0
+
+    def x(self):
+        self.x_count+=1
+
+class CleanupTest(_testcommon.HsTest):
+    
+    def test_cleanup(self):
+        cl = base.Cleanup.Cleanup()
+        
+        d = Dummy()
+        cl.push(d.x)
+        cl.clean()
+        self.assertEqual(d.x_count,1)
+
+    def test_cleanall(self):
+        cl = base.Cleanup.Cleanup()
+        
+        d = Dummy()
+        cl.push(d.x)
+        cl.push(d.x)
+        cl.clean_all()
+        self.assertEqual(d.x_count,2)
+
+    def test_double(self):
+        cl0 = base.Cleanup.Cleanup()
+        cl1 = base.Cleanup.Cleanup()
+        
+        a0 = Dummy()
+        a1 = Dummy()
+        
+        cl0.push(a0.x)
+        self.assertEqual(a0.x_count,0)
+        self.assertEqual(a1.x_count,0)
+        
+        cl1.push(a1.x)
+        self.assertEqual(a0.x_count,0)
+        self.assertEqual(a1.x_count,0)
+
+        cl1.clean_all()
+        self.assertEqual(a0.x_count,0)
+        self.assertEqual(a1.x_count,1)
+
+        cl0.clean()
+        self.assertEqual(a0.x_count,1)
+        self.assertEqual(a1.x_count,1)
