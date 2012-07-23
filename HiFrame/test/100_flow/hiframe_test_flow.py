6546189d0a43834dcc55d33e96109de37e8c7dce
luzi82 <luzi82@gmail.com>
2012-07-23 11:47:43 +0800

    add flow test stuff, rough, unfinished

---
+++ HiFrame/test/100_flow/hiframe_test_flow.py
@@ -0,0 +1,36 @@
+import unittest
+from hiframe import hiframe
+import os
+import shutil
+
+MY_ABSOLUTE_PATH = os.path.abspath(__file__)
+MY_ABSOLUTE_PARENT = os.path.dirname(MY_ABSOLUTE_PATH)
+DATA_PATH = MY_ABSOLUTE_PATH+"/data"
+
+class HiframeTestFlow(unittest.TestCase):
+    
+    def setUp(self):
+        shutil.rmtree(DATA_PATH)
+
+    def test_flow_0(self):
+        hf = hiframe.HiFrame(plugin_path_list=[MY_ABSOLUTE_PARENT],data_path=DATA_PATH)
+        hf.build()
+        hf.start()
+        hf.end()
+        hf.clean()
+
+    def test_flow_1(self):
+        hf = hiframe.HiFrame(plugin_path_list=[MY_ABSOLUTE_PARENT],data_path=DATA_PATH)
+        hf.build()
+        hf.clean()
+
+    def test_build_twice(self):
+        hf = hiframe.HiFrame(plugin_path_list=[MY_ABSOLUTE_PARENT],data_path=DATA_PATH)
+        hf.build()
+        hf.build()
+
+    def test_clean_twice(self):
+        hf = hiframe.HiFrame(plugin_path_list=[MY_ABSOLUTE_PARENT],data_path=DATA_PATH)
+        hf.build()
+        hf.clean()
+        hf.clean()
