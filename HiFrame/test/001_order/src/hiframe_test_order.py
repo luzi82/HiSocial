2d8d2e44231c09d71dc1f4c59dcae0050bdf1b79
luzi82 <luzi82@gmail.com>
2012-07-26 13:41:40 +0800

    call order test case not passed yet

---
+++ HiFrame/test/001_order/src/hiframe_test_order.py
@@ -0,0 +1,76 @@
+import unittest
+from hiframe import hiframe
+import os
+
+MY_ABSOLUTE_PATH = os.path.abspath(__file__)
+MY_ABSOLUTE_PARENT = os.path.dirname(MY_ABSOLUTE_PATH)
+
+class HiframeTestBasic(unittest.TestCase):
+
+    def test_after_abc(self):
+        hf = hiframe.HiFrame(plugin_path_list=[MY_ABSOLUTE_PARENT])
+
+        tmp = hf.call("after_abc")
+        self.assertEqual(len(tmp),3)
+        self.assertEqual(tmp[0]["func"],"after_abc_aaa")
+        self.assertEqual(tmp[1]["func"],"after_abc_bbb")
+        self.assertEqual(tmp[2]["func"],"after_abc_ccc")
+
+    def test_after_cba(self):
+        hf = hiframe.HiFrame(plugin_path_list=[MY_ABSOLUTE_PARENT])
+
+        tmp = hf.call("after_cba")
+        self.assertEqual(len(tmp),3)
+        self.assertEqual(tmp[0]["func"],"after_cba_ccc")
+        self.assertEqual(tmp[1]["func"],"after_cba_bbb")
+        self.assertEqual(tmp[2]["func"],"after_cba_aaa")
+
+    def test_after2(self):
+        hf = hiframe.HiFrame(plugin_path_list=[MY_ABSOLUTE_PARENT])
+
+        tmp = hf.call("after2")
+        self.assertEqual(len(tmp),3)
+        self.assertEqual(tmp[2]["func"],"after2_bbb")
+
+    def test_before_abc(self):
+        hf = hiframe.HiFrame(plugin_path_list=[MY_ABSOLUTE_PARENT])
+
+        tmp = hf.call("before_abc")
+        self.assertEqual(len(tmp),3)
+        self.assertEqual(tmp[0]["func"],"before_abc_aaa")
+        self.assertEqual(tmp[1]["func"],"before_abc_bbb")
+        self.assertEqual(tmp[2]["func"],"before_abc_ccc")
+
+    def test_before_cba(self):
+        hf = hiframe.HiFrame(plugin_path_list=[MY_ABSOLUTE_PARENT])
+
+        tmp = hf.call("before_cba")
+        self.assertEqual(len(tmp),3)
+        self.assertEqual(tmp[0]["func"],"before_cba_ccc")
+        self.assertEqual(tmp[1]["func"],"before_cba_bbb")
+        self.assertEqual(tmp[2]["func"],"before_cba_aaa")
+
+    def test_before2(self):
+        hf = hiframe.HiFrame(plugin_path_list=[MY_ABSOLUTE_PARENT])
+
+        tmp = hf.call("before2")
+        self.assertEqual(len(tmp),3)
+        self.assertEqual(tmp[0]["func"],"before2_bbb")
+
+    def test_afterbefore_abc(self):
+        hf = hiframe.HiFrame(plugin_path_list=[MY_ABSOLUTE_PARENT])
+
+        tmp = hf.call("afterbefore_abc")
+        self.assertEqual(len(tmp),3)
+        self.assertEqual(tmp[0]["func"],"afterbefore_abc_aaa")
+        self.assertEqual(tmp[1]["func"],"afterbefore_abc_bbb")
+        self.assertEqual(tmp[2]["func"],"afterbefore_abc_ccc")
+
+    def test_afterbefore_cba(self):
+        hf = hiframe.HiFrame(plugin_path_list=[MY_ABSOLUTE_PARENT])
+
+        tmp = hf.call("afterbefore_cba")
+        self.assertEqual(len(tmp),3)
+        self.assertEqual(tmp[0]["func"],"afterbefore_cba_ccc")
+        self.assertEqual(tmp[1]["func"],"afterbefore_cba_bbb")
+        self.assertEqual(tmp[2]["func"],"afterbefore_cba_aaa")
---
+++ HiFrame/test/001_order/src/hiframe_test_order_plugin/_hiframe.py
@@ -0,0 +1,117 @@
+def after_abc_aaa():
+    pass
+
+def after_abc_bbb():
+    pass
+
+def after_abc_ccc():
+    pass
+
+after_abc_aaa.key_list=[{"id":"after_abc"}]
+after_abc_bbb.key_list=[{"id":"after_abc","after":[after_abc_aaa]}]
+after_abc_ccc.key_list=[{"id":"after_abc","after":[after_abc_bbb]}]
+
+###################
+
+def after_cba_aaa():
+    pass
+
+def after_cba_bbb():
+    pass
+
+def after_cba_ccc():
+    pass
+
+after_cba_aaa.key_list=[{"id":"after_cba","after":[after_cba_bbb]}]
+after_cba_bbb.key_list=[{"id":"after_cba","after":[after_cba_ccc]}]
+after_cba_ccc.key_list=[{"id":"after_cba"}]
+
+###################
+
+def after2_aaa():
+    pass
+
+def after2_bbb():
+    pass
+
+def after2_ccc():
+    pass
+
+after2_aaa.key_list=[{"id":"after2"}]
+after2_bbb.key_list=[{"id":"after2","after":[after2_aaa,after2_ccc]}]
+after2_ccc.key_list=[{"id":"after2"}]
+
+###################
+
+def before_abc_aaa():
+    pass
+
+def before_abc_bbb():
+    pass
+
+def before_abc_ccc():
+    pass
+
+before_abc_aaa.key_list=[{"id":"before_abc","before":[before_abc_bbb]}]
+before_abc_bbb.key_list=[{"id":"before_abc","before":[before_abc_ccc]}]
+before_abc_ccc.key_list=[{"id":"before_abc"}]
+
+###################
+
+def before_cba_aaa():
+    pass
+
+def before_cba_bbb():
+    pass
+
+def before_cba_ccc():
+    pass
+
+before_cba_aaa.key_list=[{"id":"before_cba"}]
+before_cba_bbb.key_list=[{"id":"before_cba","before":[before_cba_aaa]}]
+before_cba_ccc.key_list=[{"id":"before_cba","before":[before_cba_bbb]}]
+
+###################
+
+def before2_aaa():
+    pass
+
+def before2_bbb():
+    pass
+
+def before2_ccc():
+    pass
+
+before2_aaa.key_list=[{"id":"before2"}]
+before2_bbb.key_list=[{"id":"before2","before":[before2_aaa,before2_bbb]}]
+before2_ccc.key_list=[{"id":"before2"}]
+
+###################
+
+def afterbefore_abc_aaa():
+    pass
+
+def afterbefore_abc_bbb():
+    pass
+
+def afterbefore_abc_ccc():
+    pass
+
+afterbefore_abc_aaa.key_list=[{"id":"afterbefore_abc"}]
+afterbefore_abc_bbb.key_list=[{"id":"afterbefore_abc","after":[afterbefore_abc_aaa],"before":[afterbefore_abc_ccc]}]
+afterbefore_abc_ccc.key_list=[{"id":"afterbefore_abc"}]
+
+###################
+
+def afterbefore_cba_aaa():
+    pass
+
+def afterbefore_cba_bbb():
+    pass
+
+def afterbefore_cba_ccc():
+    pass
+
+afterbefore_cba_aaa.key_list=[{"id":"afterbefore_cba"}]
+afterbefore_cba_bbb.key_list=[{"id":"afterbefore_cba","after":[afterbefore_cba_ccc],"before":[afterbefore_cba_aaa]}]
+afterbefore_cba_ccc.key_list=[{"id":"afterbefore_cba"}]
